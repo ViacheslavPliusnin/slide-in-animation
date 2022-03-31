a49e36e4512b2d2ae0d2d2c65c9bf25056fa4d43
Viacheslav Pliusnin <via.plsn@gmail.com>
2022-03-31 11:13:46 +0300

    Cat gif added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
+import { white } from './constants/colors';
+import NyanCat from './assets/images/nyan-cat.gif';
+
+const Section = styled.div<{ color?: string }>`
+  width: 100%;
+  min-height: 500px;
+  background-color: ${({ color }) => color || white};
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!!!</h1>
-    </div>
+    <Section>
+      <img src={NyanCat} alt="Nyan Cat gif" />
+    </Section>
   );
 }
 
